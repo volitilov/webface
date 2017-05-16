7650151151d2780692853f24a877a8b7ca19e16d
volitilov <volitilov@gmail.com>
2017-05-16 22:39:25 +0300

    new changes

--- mysite/views.py
+++ mysite/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post, Mesage, PortfolioItem
-import random
-
 
 
 def indexView(request):
